264831f074960ac1e5ac3e332fb3e2a9ff62a3cd
Mohammad Aslam <aslampaasa420@gmail.com>
2025-10-18 20:53:36 +0530

    add two nums

---
+++ _01_getting_started/_04_add_two_nums.java
@@ -0,0 +1,11 @@
+package _01_getting_started;
+
+public class _04_add_two_nums {
+    public static void main(String[] args) {
+        int x = 80;
+        int y = 90;
+        int z = x + y;
+
+        System.out.println(z);
+    }
+}
