33b83a6691e8e1342316e0cac8934fdd0e3737d8
Mohammad Aslam <aslampaasa420@gmail.com>
2025-10-18 21:50:00 +0530

    print 1 to N

---
+++ _03_loop_basics/_01_print_1_to_n.java
@@ -0,0 +1,11 @@
+package _03_loop_basics;
+
+public class _01_print_1_to_n {
+    public static void main(String[] args) {
+        int n = 10;
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i);
+        }
+    }
+}
