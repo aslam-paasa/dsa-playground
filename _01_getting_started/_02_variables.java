62286526d7da3d16179a77a0df6155fcfce29643
Mohammad Aslam <aslampaasa420@gmail.com>
2025-10-18 20:49:05 +0530

    variables

---
+++ _01_getting_started/_02_variables.java
@@ -0,0 +1,33 @@
+package _01_getting_started;
+
+/**
+ * DataTypes:
+ * 1. Integers: 9, 1000, -22
+ * 2. Float: 19.23, 7.12, 4.09
+ * 3. Double: 19.2378645654, 7.123122321, 4.097332734482
+ * 4. Character: ('$', '%', '*', '/', 'a', 'n')
+ * 5. String: ("$%*6/an")
+ * 6. Boolean: (True, False)
+ */
+
+public class _02_variables {
+    public static void main(String[] args) {
+        int a = 7;
+        System.out.println(a);
+
+        float b = 7.89f;
+        System.out.println(b);
+
+        double c = 7.132321;
+        System.out.println(c);
+
+        char d = 'a';
+        System.out.println(d);
+
+        String e = "Hello";
+        System.out.println(e);
+
+        boolean f = true;
+        System.out.println(f);
+    }
+}
