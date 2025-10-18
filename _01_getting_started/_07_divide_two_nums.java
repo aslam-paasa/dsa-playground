f0255615e108f918b83835094e84c7f46a000dee
Mohammad Aslam <aslampaasa420@gmail.com>
2025-10-18 20:57:37 +0530

    divide two nums

---
+++ _01_getting_started/_07_divide_two_nums.java
@@ -0,0 +1,11 @@
+package _01_getting_started;
+
+public class _07_divide_two_nums {
+    public static void main(String[] args) {
+        int a = 13;
+        int b = 3;
+        int c = a/b;
+
+        System.out.println(c);
+    }
+}
