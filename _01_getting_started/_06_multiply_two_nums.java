bd813c4247de1d47916b2d01e630d20e681e7e9b
Mohammad Aslam <aslampaasa420@gmail.com>
2025-10-18 20:56:41 +0530

    multiply two nums

---
+++ _01_getting_started/_06_multiply_two_nums.java
@@ -0,0 +1,11 @@
+package _01_getting_started;
+
+public class _06_multiply_two_nums {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 2;
+        int c = a*b;
+
+        System.out.println(c);
+    }
+}
